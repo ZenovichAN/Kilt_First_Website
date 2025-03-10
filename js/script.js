818e5aa55dfee1c3d0ae2268636013777578d9a1
Anton Zenovich <zenovichan@gmail.com>
2025-03-10 20:55:33 +0700

    I'm added folder with js

---
+++ js/script.js
@@ -0,0 +1,10 @@
+'use strict';//чтобы весь код был в строгом режиме
+
+console.log('Hello epta');
+let message;
+message = 'Hello epta';
+console.log(message);
+message = 'fuck';
+console.log(message);
+
+const messages = "by";
